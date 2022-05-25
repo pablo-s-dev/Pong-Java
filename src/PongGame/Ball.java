a51b94d527d97b618ff238202646fe07db48545f
pablos-dev <svolkito@gmail.com>
2022-05-25 15:57:53 -0300

    solid wall

--- src/PongGame/Ball.java
+++ src/PongGame/Ball.java
@@ -15,34 +15,74 @@ public class Ball extends JComponent{
 	public static int BALL_HEIGHT = 10;
 	public static int CENTRO_X = Panel.GAME_WIDTH/2-BALL_WIDTH/2;
 	public static int CENTRO_Y = Panel.GAME_HEIGHT/2-BALL_HEIGHT/2;
-	private double tetha = (double) Math.toRadians(270);
+	private double tetha;
+	private double rad;
 	private double xIncrement;
 	private double yIncrement;
+	private static boolean wasGoingDown;
+	private static  boolean rebouncedByWall;
 	Random rand = new Random();
 	Ball(){
-		this.setPreferredSize(new Dimension(BALL_WIDTH, BALL_HEIGHT));
+		tetha = 270;
+		rad = 3*Math.PI/2;
+		xIncrement = 0;
+		yIncrement = 0;
+		wasGoingDown = true;
+		rebouncedByWall = false;
+		//this.setPreferredSize(new Dimension(BALL_WIDTH, BALL_HEIGHT));
 	}
 	
 	public void drawBall(Graphics2D g2d) {
-			System.out.println(xIncrement);
-			xIncrement +=  Math.cos(tetha);
-			yIncrement -=  Math.sin(tetha);
-			//System.out.println(xIncrement);
-			ballDrawing = new Rectangle2D.Double(CENTRO_X + xIncrement, CENTRO_Y + yIncrement, BALL_WIDTH, BALL_WIDTH);
-		
 			
-			
-			
-			
-			if(ballDrawing.contains((Player.drawPlayer(g2d, 1)))){  //colisao
-				tetha = rand.nextInt(120)+30+180; 
-			}
-			if(ballDrawing.contains((Player.drawPlayer(g2d, 2)))){ //colisao
-				tetha = rand.nextInt(120)+30+180;
-			}
-			
-			
+			xIncrement +=  3* Math.cos(rad);
+			yIncrement -=  3*Math.sin(rad);
+			ballDrawing = new Rectangle2D.Double(CENTRO_X + xIncrement, CENTRO_Y + yIncrement, BALL_WIDTH, BALL_HEIGHT);
 
+			
+			Rectangle2D.Double desenhoDoPlayer1 = (Player.drawPlayer(g2d, 1));
+			Rectangle2D.Double desenhoDoPlayer2 = (Player.drawPlayer(g2d, 2));
+			
+			//top collision
+			if(desenhoDoPlayer1.intersects(ballDrawing) && wasGoingDown == false){ 
+				tetha = rand.nextInt(100)+40+180;
+				rad = Math.toRadians(tetha);
+				wasGoingDown = true;
+				rebouncedByWall = false;
+			}
+			//bottom collision
+			if(desenhoDoPlayer2.intersects(ballDrawing) && wasGoingDown == true){
+				tetha = rand.nextInt(100)+40;
+				rad = Math.toRadians(tetha);
+				wasGoingDown = false;
+				rebouncedByWall = false;
+			}
+			//left wall collision
+			if(ballDrawing.intersectsLine(0, 0, 0, Panel.GAME_HEIGHT) && rebouncedByWall == false){
+				rebouncedByWall = true;
+				tetha = 180 - tetha;
+				rad = Math.toRadians(tetha);
+			}
+			//right wall collision
+			if(ballDrawing.intersectsLine(Panel.GAME_WIDTH - Ball.BALL_WIDTH, 0, Panel.GAME_WIDTH - Ball.BALL_WIDTH, Panel.GAME_HEIGHT) 
+					&& rebouncedByWall == false){
+				rebouncedByWall = true;
+				tetha = 180 - tetha;
+				rad = Math.toRadians(tetha);
+			}
+			
+			///////////// PONTOS //////////////////
+			if(ballDrawing.y < - Ball.BALL_HEIGHT - 10) {
+				System.out.println("Ponto do player de baixo");
+				Panel.bottomPoints++;
+				System.out.println(Panel.bottomPoints);
+				Panel.Restart();
+			}
+			if(ballDrawing.y > Panel.GAME_HEIGHT + Ball.BALL_HEIGHT) {
+				System.out.println("Ponto do player de cima");
+				Panel.topPoints++;
+				System.out.println(Panel.topPoints);
+				Panel.Restart();
+			}
 			g2d.setColor(new Color(208, 207, 207));
 			g2d.fill(ballDrawing);		
 	}
--- src/PongGame/Panel.java
+++ src/PongGame/Panel.java
@@ -7,26 +7,38 @@ import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel implements Runnable, KeyListener{
-	Player player1 = new Player();
-	Player player2 = new Player();
+public class Panel extends JPanel implements Runnable{
+	static Player player1 = new Player();
+	static Player player2 = new Player();
 	public static final int GAME_WIDTH = 400;
 	public static final int GAME_HEIGHT = 700;
 	//public static final int SCALE = 3;
 	public static final Dimension GAME_DIMENSION = new Dimension(GAME_WIDTH, GAME_HEIGHT);
-	Ball ball = new Ball();
-	
-	
+	static Ball ball = new Ball();
+	public static boolean isRunning = true;
+	public static int bottomPoints;
+	public static int topPoints;
+	JLabel textPoints = new JLabel("Alo");
 	Panel(){
-		this.addKeyListener(this);
+		
 		this.setBackground(new Color(40, 40, 40));
+		this.setLayout(null);
 		this.setMinimumSize(GAME_DIMENSION);
 		this.setPreferredSize(GAME_DIMENSION);
 		this.add(player1);
 		this.add(player2);
 		this.add(ball);
+		textPoints.setSize(20, 100);
+		textPoints.setBounds(0, GAME_HEIGHT/2, 200, 100);
+		textPoints.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+		textPoints.setAlignmentY(JLabel.CENTER_ALIGNMENT);
+		textPoints.setVisible(true);
+		textPoints.setText("ALO");
+		textPoints.setForeground(Color.white);
+		this.add(textPoints);
 		
 	}
 	
@@ -35,42 +47,35 @@ public class Panel extends JPanel implements Runnable, KeyListener{
 		g2d.setColor(new Color(40,40,40));
 		super.paintComponent(g2d);
 		ball.drawBall(g2d);
+		
 	}
 
 	@Override
 	public void run() {
 		double pastTime = System.nanoTime(); //salvando um instante de tempo
-		double timePerFrame = 1000000000/60.0; //duracao em nano segundos de cada frame
+		double timePerFrame = 1000000000/100; //duracao em nano segundos de cada frame
 		//System.out.println("60");
-		while(true) {
+		while(Panel.isRunning) {
 			double currentTime = System.nanoTime();  //salvando outro instante de tempo
 			if((currentTime- pastTime) >= timePerFrame){
 				repaint();
 				//System.out.println("novo frame");
 				pastTime = currentTime;
+				
 			}
 		}
 		
 		
 		
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		System.out.println("PAOZINHO");
-		
+	static void Restart(){
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ball = new Ball();
 	}
 
 }
